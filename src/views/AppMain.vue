503e92152dca978847d848cc75aeff6fa699f8e9
조용훈 <sjsin0905@naver.com>
2024-05-15 23:06:03 +0900

    [feat]index

--- src/views/AppMain.vue
+++ src/views/AppMain.vue
@@ -1,10 +1,33 @@
 <script setup>
 import Header from "@/components/Header.vue";
 import Footer from "@/components/Footer.vue";
+import MainBackGround from "@/components/main/MainBackGround.vue";
 </script>
 
 <template>
   <Header />
-  <div><a href="http://localhost:5173/loan">aa</a></div>
+  <div>
+    <MainBackGround></MainBackGround>
+    <div id="main-content">
+      <div id="box"></div>
+    </div>
+  </div>
   <Footer />
 </template>
+<style>
+body {
+  margin: 0;
+}
+#main-content {
+  width: 69vw;
+  height: 100vh;
+  background-color: aqua;
+  padding-left: 15vw;
+  padding-right: 15vw;
+}
+#box {
+  width: 100%;
+  height: 100%;
+  background-color: aquamarine;
+}
+</style>
