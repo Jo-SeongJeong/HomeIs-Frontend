ff393eee03d234ebd7f1daf39e73bec972ab5408
sjsin0905 <sjsin0905@naver.com>
2024-05-21 17:54:56 +0900

    feat/homesta-detail

--- src/views/Homestagram/HomestagramDetail.vue
+++ src/views/Homestagram/HomestagramDetail.vue
@@ -2,6 +2,21 @@
 import { ref } from "vue";
 import { useRoute, useRouter } from "vue-router";
 import boardApi from "@/api/boardApi";
+
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from "swiper/vue";
+
+// Import Swiper styles
+import "swiper/css";
+
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+
+// import required modules
+import { EffectCoverflow, Pagination } from "swiper/modules";
+
+const modules = [EffectCoverflow, Pagination];
+
 const route = useRoute();
 const router = useRouter();
 const id = route.params.id;
@@ -67,37 +82,118 @@ const isWriteUser = () => {
 </script>
 
 <template>
-  <div>
-    <h2>{{ homesta.title }}</h2>
-    <div>좋아요 : {{ homesta.totalLike }}</div>
-    <button @click="addLike()" v-if="homesta.isLike == 0">좋아용!!</button>
-    <button type="button" @click="addLike()" v-else-if="homesta.isLike == 1">
-      좋아용 취소!!
-    </button>
-    <div>조회수 : {{ homesta.view }}</div>
-    <button type="button" @click="deleteHomesta()" v-if="isWriteUser()">
-      삭제
-    </button>
-    <div class="img-div">
-      <div v-for="imgInfo in homesta.image" :key="imgInfo.saveName">
-        <div
-          style="width: 150px; height: 200px"
-          :style="{
-            backgroundImage: `url(${getImgUrl(imgInfo.saveName)})`,
-          }"
-        ></div>
+  <div id="homesta-detail-main">
+    <div id="homesta-detail-header">
+      <div id="homesta-detail-header-title">
+        <h2>{{ homesta.title }}</h2>
       </div>
+      <div style="font-size: 1.2rem">{{ homesta.view }} &#128064; views</div>
+      <div v-if="homesta.isLike == 0" style="font-size: 1.2rem">
+        <a>{{ homesta.totalLike }} </a
+        ><a @click="addLike()" style="cursor: pointer">
+          &nbsp;<i class="fa-regular fa-heart" style="color: #ff1100"></i>
+          Likes</a
+        >
+      </div>
+      <div v-else-if="homesta.isLike == 1" style="font-size: 1.2rem">
+        <a>{{ homesta.totalLike }} </a>
+        <a @click="addLike()" style="cursor: pointer">
+          &nbsp;<i class="fa-solid fa-heart" style="color: #ff0000"></i>
+          Likes</a
+        >
+      </div>
+      <a @click="deleteHomesta()" v-if="isWriteUser()" style="cursor: pointer">
+        삭제
+      </a>
+    </div>
+    <div id="homesta-detail-content">
+      <div id="homesta-detail-swiper">
+        <swiper
+          :effect="'coverflow'"
+          :centeredSlides="true"
+          :slidesPerView="'auto'"
+          :coverflowEffect="{
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+          }"
+          :pagination="{
+            clickable: true,
+          }"
+          :modules="modules"
+          class="mySwiper"
+        >
+          <swiper-slide v-for="imgInfo in homesta.image" :key="imgInfo.saveName"
+            ><div
+              id="homesta-detail-image-box"
+              :style="{
+                backgroundImage: `url(${getImgUrl(imgInfo.saveName)})`,
+              }"
+            ></div>
+          </swiper-slide>
+        </swiper>
+      </div>
+      <div>
+        <a>{{ homesta.content }}</a>
+      </div>
+      ✍ {{ homesta.userId }}
     </div>
   </div>
-
-  <div>해시태그 {{ homesta.content }}</div>
 </template>
 
 <style scoped>
-.img-div {
+.mySwiper {
+  width: 100%;
+  height: 100%;
+}
+.swiper-slide {
+  background-position: center;
+  background-size: cover;
+  width: 70%;
+  height: 100%;
+}
+#homesta-detail-main {
+  width: 100vw;
+  height: 70vh;
+  padding-left: 20vw;
+  padding-right: 20vw;
+  #homesta-detail-header {
+    width: 100%;
+    height: 20%;
+    display: flex;
+    flex-direction: row;
+    align-items: end;
+    gap: 1vw;
+    #homesta-detail-header-title {
+      width: 75%;
+      padding-left: 2vw;
+      font-size: 1.8rem;
+    }
+  }
+}
+#homesta-detail-content {
+  width: 100%;
+  height: 80%;
   display: flex;
-  flex-direction: row;
-  justify-content: space-around;
+  flex-direction: column;
+  justify-content: center;
   align-items: center;
+  gap: 2vw;
+  font-size: 1.5rem;
+  a {
+    border-radius: 6px;
+  }
+  #homesta-detail-swiper {
+    width: 60%;
+    height: 70%;
+  }
+}
+#homesta-detail-image-box {
+  width: 100%;
+  height: 100%;
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
 }
 </style>
